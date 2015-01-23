af738a7fb9e61b7fa8eb5bfe4dbddfae9cf8562c
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-01-23 13:00:03 +0530

    new Tests are added for findIndex(), findFirst() method written by mahesh.

--- maheshTest.c
+++ maheshTest.c
@@ -29,3 +29,23 @@ void test_findFirst_returns_the_address_of_the_first_element_in_the_array_that_i
     ArrayUtil util1 = {array1, sizeof(char), 3};
     assertEqual(*(char *)(findFirst(util1,&isCapital,'a')), 'M');
 }
+
+void test_findFirst_returns_the_adsdress_of_first_element_in_the_array_greater_than_5() {
+    float array1[] = {1.1,6.6,5.5,8.8,2.2};
+    ArrayUtil util1 ={array1, sizeof(float),5};
+    assertEqual(*(float *)(findFirst(util1,&isGreaterThan5,0)), (float)6.6);
+}
+
+void test_findIndex_returns_index_of_the_String_element_where_it_presents(){
+    String array1[] = {"Mahesh","Kumar","Kolla"};
+    String element = "Kumar";
+    ArrayUtil util1 = {array1,sizeof(String),3};
+    assertEqual(findIndex(util1,&element), 1);
+}
+
+void test_findIndex_returns_index_of_the_char_element_where_it_presents(){
+    char array1[] = {'m','a','h','e','s','h'};
+    char element = 's';
+    ArrayUtil util1 = {array1,sizeof(char),6};
+    assertEqual(findIndex(util1,&element), 4);
+}
