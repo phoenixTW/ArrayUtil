daa1ee07d1aeca2abdceb33489cb55ecb99fb9f5
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-01-23 11:22:58 +0530

    new Test is added for create method written by Vijay. Now the total test number is #49

--- dollyTest.c
+++ dollyTest.c
@@ -13,11 +13,3 @@ void test_findIndex_returns_1_if_the_search_char_is_found_on_1st_location (){
     util.base = (void*)array;
     assertEqual(findIndex(util,&x),1);
 }
-
-void* sum (void* hint, void* pv, void* cv){
-    int* _pv = (int*)pv;
-    int* _cv = (int*)cv;
-    int* result = (int*)malloc(INT_SIZE); 
-    *result = *_pv + *_cv;
-    return result;
-}
---
+++ vijayTest.c
@@ -0,0 +1,12 @@
+#include "expr_assert.h"
+#include "arrayUtil.h"
+
+void test_that_function_creates_new_array_or_not(){
+    ArrayUtil array = create(sizeof(int),3);
+    int *createArray  = (int*)array.base;
+    assertEqual(createArray[0], 0);
+    assertEqual(createArray[2], 0);
+    assertEqual(array.length, 3);
+    assertEqual(array.typeSize, 4);
+    dispose(array);
+}
